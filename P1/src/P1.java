d78b7d7d90e47ff4bf5da5cd3c6889571a92a802
Jordan Cox <jordancox747@outlook.com>
2019-01-22 15:03:15 -0700

    P1 commit

---
+++ P1/src/P1.java
@@ -0,0 +1,21 @@
+
+// P1 Assignment
+// Author: Cox, Jordan
+// Date:   01/22/2019
+// Class:  CS164
+// Email:  jordancc@rams.colostate.edu
+
+public class P1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java programming is great!");
+        System.out.println("Cox, Jordan");
+        System.out.println("Computer Science");
+        System.out.println(1234*2);
+        System.out.println("01/28/2019");
+        System.out.println("CS163/4: CS1 Java");
+        System.out.print("I am taking this class because I'm a CS major and it's required.");
+
+    }
+}
